503e66416be45c4da151fe9ca990d0d315431d3e
Wanderley <100Palavr@gmail.com>
2021-11-16 19:26:24 -0300

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,62 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct no {
+typedef struct Pilha {
     int val;
-    struct no* ant;
-} No;
-
-typedef struct pilha {
-    No* final;
+    struct Pilha* ant;
 } Pilha;
 
-Pilha* inicializar() {
-    Pilha* aux = (Pilha*)malloc(sizeof(Pilha));
-    aux->final = NULL;
-    return aux;
+Pilha* inicializar(int val) {
+    Pilha* temp = (Pilha*)malloc(sizeof(Pilha));
+    temp->val   = val;
+    temp->ant   = NULL;
+    return temp;
 }
 
-No* criar_no(int val) {
-    No* aux   = (No*)malloc(sizeof(No));
-    aux->val  = val;
-    aux->ant  = NULL;
-    return aux;
-}
-
-void enfileirar(Pilha** ptr, int val) {
-    if((*ptr)->final == NULL) {
-        (*ptr)->final = criar_no(val);
+void inserir(Pilha** pl, int val) {
+    if((*pl) == NULL) {
+        (*pl) = inicializar(val);
         return;
     }
 
-    No* aux  = criar_no(val);
-    aux->ant = (*ptr)->final;
-    (*ptr)->final = aux;
+    Pilha* aux = inicializar(val);
+    aux->ant = (*pl);
+    (*pl) = aux;
 }
 
-int desenfileirar(Pilha** ptr) {
-    if((*ptr)->final != NULL) {
-        No* aux       = (*ptr)->final;
-        (*ptr)->final = (*ptr)->final->ant;
-        free(aux);
-        return 1;
+int remover(Pilha** pl) {
+    int val    = (*pl)->val;
+    Pilha* aux = (*pl);
+    (*pl)      = (*pl)->ant;
+    free(aux);
+    return val;
+}
+
+int top(Pilha* pl) {
+    return pl->val;
+}
+
+int vazio(Pilha* pl) {
+    return pl == NULL;
+}
+
+int main() {
+    Pilha* pl = NULL;
+    inserir(&pl, 1);
+    inserir(&pl, 2);
+    inserir(&pl, 3);
+    inserir(&pl, 4);
+    inserir(&pl, 5);
+
+    while(!vazio(pl)) {
+        printf("%d\n", remover(&pl));
     }
-    return 0;
-}
 
-int obter(Pilha** ptr) {
-    if((*ptr)->final != NULL) {
-        return (*ptr)->final->val;
-    }
-}
-
-void liberar_pilha(Pilha** pl) {
-    while(desenfileirar(pl));
-}
-
-int pilha_vazia(Pilha** pl) {
-    if((*pl)->final == NULL) {
-        return 1;
-    }
     return 0;
 }
