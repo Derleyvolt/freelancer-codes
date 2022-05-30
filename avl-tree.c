e4f54a673c64f3607847e568517f5a843278c291
Wanderley <100Palavr@gmail.com>
2022-05-29 22:42:54 -0300

    Update avl-tree.c

--- avl-tree.c
+++ avl-tree.c
@@ -30,8 +30,8 @@ Arv* rotacao_dir(Arv* raiz) {
     raiz->esq  = aux->dir;
     aux->dir   = raiz;
 
-    raiz->altura  = altura(raiz);
-    aux->altura   = altura(aux);
+    raiz->altura = altura(raiz);
+    aux->altura  = altura(aux);
     return aux;
 }
 
@@ -45,6 +45,44 @@ Arv* rotacao_esq(Arv* raiz) {
     return aux;
 }
 
+Arv* rotacao_esq_dir(Arv* raiz) {
+    raiz->esq = rotacao_esq(raiz->esq);
+    //Arv* aux = rotacao_esq(raiz->esq);
+    return rotacao_dir(raiz);
+}
+
+Arv* rotacao_dir_esq(Arv* raiz) {
+    raiz->dir = rotacao_dir(raiz->dir);
+    //Arv* aux = rotacao_dir(raiz->dir);
+    return rotacao_esq(raiz);
+}
+
+int balanceamento(Arv* root) {
+    if(root == NULL)
+        return 0;
+    return altura(root->esq) - altura(root->dir);
+}
+
+int obter_altura(Arv* raiz) {
+    if(raiz == NULL) {
+        return 0;
+    }
+
+    return raiz->altura;
+}
+
+Arv* encontrar_min(Arv* root) {
+    if(root == NULL) {
+        return NULL;
+    }
+
+    if(root->esq == NULL) {
+        return root;
+    }
+
+    encontrar_min(root->esq);
+}
+
 Arv* inserir(Arv* root, int dado) {
     if(root == NULL) {
         return criar_no(dado);
@@ -56,28 +94,83 @@ Arv* inserir(Arv* root, int dado) {
         // root precisa atualizar root->esq pra apontar pra
         // um novo nó (a nova raiz da subarvore apontada por ele)
         root->esq = inserir(root->esq, dado);
-        if(abs(altura(root->esq) - altura(root->dir)) >= 2) {
+
+        if(abs(balanceamento(root)) >= 2) {
             // o desbalanceamento com ctz aconteceu na subárvore
             // à esquerda
-            root = rotacao_dir(root);
+            if(balanceamento(root->esq) == -1) {
+                root = rotacao_esq_dir(root);
+            } else {
+                root = rotacao_dir(root);
+            }
         }
     } else if(root->dado < dado) {
         root->dir = inserir(root->dir, dado);
 
-        if(abs(altura(root->esq) - altura(root->dir)) >= 2) {
+        if(abs(balanceamento(root)) >= 2) {
             // o desbalanceamento com ctz aconteceu na subárvore
             // à direita
-            root = rotacao_esq(root);
+            if(balanceamento(root->dir) == 1) {
+                root = rotacao_dir_esq(root);
+            } else {
+                root = rotacao_esq(root);
+            }
         }
     }
-
+    
     // atualizo a altura e retorno
-    root->altura = altura(root);
+    root->altura = max(obter_altura(root->dir), obter_altura(root->esq)) + 1;
     return root;
 }   
 
-void remover(Arv** arv, int dado) {
-    
+Arv* remover(Arv** arv, int dado) {
+    Arv* root = *arv;
+    if(root == NULL) {
+        return NULL;
+    }
+
+    if(root->dado > dado) {
+        root->esq = remover(&root->esq, dado);
+        if(abs(balanceamento(root)) >= 2) {
+            // sei que removi alguém da minha direita 
+            if(balanceamento(root->dir) == 1) {
+                root = rotacao_dir_esq(root);
+            } else {
+                root = rotacao_esq(root);
+            }
+        }
+    } else if(root->dado < dado) {
+        root->dir = remover(&root->dir, dado);
+        if(abs(balanceamento(root)) >= 2) {
+            // sei que removi alguém da minha direita
+            if(balanceamento(root->esq) == -1) {
+                root = rotacao_esq_dir(root);
+            } else {
+                root = rotacao_dir(root);
+            }
+        }
+    } else {
+        if(root->esq == NULL || root->dir == NULL) {
+            if(root->esq == NULL) {
+                Arv* aux = root->dir;
+                free(root);
+                return aux;
+            }
+
+            if(root->dir == NULL) {
+                Arv* aux = root->esq;
+                free(root);
+                return aux;
+            }
+        }
+
+        Arv* aux   = encontrar_min(root->dir);
+        root->dado = aux->dado;
+        root->dir  = remover(&root->dir, root->dado);
+    }
+
+    root->altura = max(obter_altura(root->dir), obter_altura(root->esq)) + 1;
+    return root;
 }
 
 void percorrer(Arv* root) {
@@ -87,12 +180,26 @@ void percorrer(Arv* root) {
     percorrer(root->dir);
 }   
 
+void percorrer_in_order(Arv* root) {
+    if(root == NULL) return;
+    printf("%d\n", root->dado);
+    percorrer_in_order(root->esq);
+    percorrer_in_order(root->dir);
+}
+
 int main() {
     Arv* arv = NULL;
-    for(int i = 0; i < 200; i++) {
+
+    for(int i = 0; i < 3000; i++) {
         arv = inserir(arv, i+1);
     }
 
-    percorrer(arv);
+    for(int i = 1; i < 3000; i++) {
+        if(i != 200)
+            arv = remover(&arv, i+1);
+    }
+
+    printf("Altura: %d\n\n", obter_altura(arv));
+    percorrer_in_order(arv);
     return 0;
 }
