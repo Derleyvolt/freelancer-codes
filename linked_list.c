ed0e43869e5192c30d8d2dfd33c35642f530ce31
Wanderley <100Palavr@gmail.com>
2022-06-05 23:18:10 -0300

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct s_list {
+typedef struct singly_linked_list {
     int val;
-    struct s_list* prox;
-} s_list;
+    struct singly_linked_list* prox;
+} singly_linked_list;
 
-s_list* create_node(int val) {
-    s_list* node = (s_list*)malloc(sizeof(s_list));
+singly_linked_list* create_node(int val) {
+    singly_linked_list* node = (singly_linked_list*)malloc(sizeof(singly_linked_list));
     node->val    = val;
     node->prox   = NULL;
     return node;
 }
 
-void push_front(s_list** lista, int val) {
-    if(*lista == NULL) {
-        (*lista) = create_node(val);
+void push_front(singly_linked_list** list, int val) {
+    if(*list == NULL) {
+        (*list) = create_node(val);
     } else {
-        s_list* aux   = create_node(val);
+        singly_linked_list* aux   = create_node(val);
         int a         = aux->val;
-        aux->val      = (*lista)->val;
-        (*lista)->val = a;
+        aux->val      = (*list)->val;
+        (*list)->val = a;
 
-        aux->prox      = (*lista)->prox;
-        (*lista)->prox = aux;
+        aux->prox      = (*list)->prox;
+        (*list)->prox = aux;
     }
 }
 
-void push_back(s_list** list, int val) {
+void push_back(singly_linked_list** list, int val) {
     if((*list) == NULL) {
         (*list) = create_node(val);
         return;
@@ -36,58 +36,56 @@ void push_back(s_list** list, int val) {
     push_back(&(*list)->prox, val);
 }
 
-int empty(s_list* list) {
+int empty(singly_linked_list* list) {
     return list == NULL;    
 }
 
-int front(s_list* list) {
+int front(singly_linked_list* list) {
     return list->val;
 }
 
-// deixa o list nula quando eu apago o último elemento
-void pop_front(s_list** list) {
+void pop_front(singly_linked_list** list) {
     if((*list) == NULL)
         return;
-    s_list* tmp = (*list);
+    singly_linked_list* tmp = (*list);
     (*list) = (*list)->prox;
     free(tmp);
 }
 
-// essa função não faz checagem quanto ao tamanho da lista
-// isso vc precisa fazer por fora, quando chamar a função
-int pos(s_list* list, int i) {
+int pos(singly_linked_list* list, int i) {
     if(i == 0) {
         return list->val;
     }
+
     return pos(list->prox, i-1);
 }
 
-s_list* erase(s_list** lista, int val) {
-    if(*lista == NULL) {
+singly_linked_list* erase(singly_linked_list** list, int val) {
+    if(*list == NULL) {
         return NULL;
     }
 
-    s_list* aux = *lista;
+    singly_linked_list* aux = *list;
 
     if(aux->val == val) {
         if(aux->prox != NULL) {
             aux->val     = aux->prox->val;
-            s_list* temp = aux->prox;
+            singly_linked_list* temp = aux->prox;
             aux->prox    = aux->prox->prox;
             free(temp);
             return aux;
         } else {
-            // este é o último elemento da lista
+            // este é o último elemento da list
             free(aux);
             return NULL;
         }
     }
 
-    aux->prox = erase(&aux->prox, e);
+    aux->prox = erase(&aux->prox, val);
     return aux;
 }
 
-void traverse(s_list* list) {
+void traverse(singly_linked_list* list) {
     if(list == NULL)
         return;
     printf("%d ", list->val);
@@ -95,13 +93,13 @@ void traverse(s_list* list) {
 }
 
 // O(n^2)
-void sort(s_list* list) {
+void sort(singly_linked_list* list) {
     if(list == NULL) {
         return;
     }
 
-    for(s_list* i = list; i != NULL; i = i->prox) {
-        for(s_list* j = i->prox; j != NULL; j = j->prox) {
+    for(singly_linked_list* i = list; i != NULL; i = i->prox) {
+        for(singly_linked_list* j = i->prox; j != NULL; j = j->prox) {
             if(j->val < i->val) {
                 int aux = i->val;
                 i->val  = j->val;
@@ -111,34 +109,37 @@ void sort(s_list* list) {
     }
 }
 
-s_list* l_search(s_list* ls, int val) {
+singly_linked_list* search(singly_linked_list* ls, int val) {
     if(ls == NULL) {
         return NULL;
     }
+
     if(ls->val == val) {
         return ls;
     }
-    return l_search(ls->prox, val);
+
+    return search(ls->prox, val);
 }
 
-int length(s_list* ls) {
+int length(singly_linked_list* ls) {
     if(ls == NULL) {
         return 0;
     }
+
     return 1 + length(ls->prox);
 }
 
 int main() {
-    s_list* lista = NULL;
+    singly_linked_list* list = NULL;
 
     for(int i = 10; i >= 0; i--) {
-        push_back(&lista, i);
+        push_back(&list, i);
     }
 
-    lista = erase(&lista, 8);
+    list = erase(&list, 8);
 
-    push_front(&lista, -5);
+    push_front(&list, -5);
     
-    traverse(lista);
+    traverse(list);
     return 0;
 }
